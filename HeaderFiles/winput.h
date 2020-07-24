4425ad75d2aaf77800b1c84d20d4dd532a235961
GLG-debug <german.volodin@mail.ru>
2020-07-24 21:21:52 +0400

    Добавил коды некоторых вируальных клавиш в заголовочном файле winput.h

--- HeaderFiles/winput.h
+++ HeaderFiles/winput.h
@@ -48,4 +48,43 @@ void BASE_input::keyinput(WPARM wp, LPARAM lp, UINT state)
     if(user_input) user_input(wp, lp, state);
 }
 
+/* Коды некоторых вируальных клавиш
+Символьные клавиши
+    0x30 - 0x39 клавиши 0 - 9
+    0x41 - 0x5a клавиши A - z
+    Можно использовать как if(in.vkey == 'A')
+
+Клавиши на числовой клавиатуры (numeric keypad)
+    VK_NUMPAD0 - VK_NUMPAD9 0X60 - 0x69 цифры на 0 - 9
+    VK_MULTIPLY знак умножения
+    VK_ADD знак сложения
+    VK_SUBTRACT знак вычитания
+    VK_DIVIDE знак деления
+Мышь
+    VK_LBUTTON 01 левая клавиша мыши
+    VK_RBUTTON 02 левая клавиша мыши
+    VK_MBUTTON 04 левая клавиша мыши
+    VK_XBUTTON1 05 X1 левая клавиша мыши
+    VK_XBUTTON2 06 X2 левая клавиша мыши
+Разное
+    VK_F1 - VK_F24 0x70 - 0x87 клавиши F1 - F24
+    VK_ESCAPE 0x1b клавиша ESC
+    VK_END 0x23 клавиша END
+    VK_HOME 0x24 клавиша HOME
+    VK_LEFT 0x25 клавиша стрелка влево
+    VK_UP 0x26 клавиша стрелка вверх
+    VK_RIGHT 0x27 клавиша стрелка вправо
+    VK_DOWN 0x28 клавиша стрелка вниз
+    VK_SELECT 0x29 клавиша SELECT key
+    VK_SPACE пробел
+    VK_LSHIFT 0xa0 левый шифт
+    VK_RSHIFT 0xa1 правый шифт 
+    VK_LCONTROL 0xa2 левая клавиша Ctrl
+    VK_RCONTROL 0xa3 правая клавиша Ctrl
+    VK_RETURN 0x0D клавиша ENTER
+    VK_SHIFT 0x10 клавиша SHIFT
+    VK_CONTROL 0x11 клавиша Ctrl
+    VK_MENU 0x12 клавиша Alt
+*/
+
 #endif
